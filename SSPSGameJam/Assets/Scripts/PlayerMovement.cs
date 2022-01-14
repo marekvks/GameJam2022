415add3eb01d5773bbe92ce4f15d720073b2c5e3
streetzy <57232857+streetzy@users.noreply.github.com>
2022-01-14 21:51:55 +0100

    Script

---
+++ SSPSGameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        
+    }
+}
