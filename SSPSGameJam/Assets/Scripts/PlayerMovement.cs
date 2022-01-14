62408679fc9fb79a0997cc05183721388aa76b75
Marek <84468808+marekvks@users.noreply.github.com>
2022-01-14 21:58:50 +0100

    Movement Script

--- SSPSGameJam/Assets/Scripts/PlayerMovement.cs
+++ SSPSGameJam/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,19 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
 
+    public float moveSpeed = 500f;
+
+    private float horizontal;
+    private float vertical;
+
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * moveSpeed * Time.fixedDeltaTime, vertical * moveSpeed * Time.fixedDeltaTime);
     }
 }
