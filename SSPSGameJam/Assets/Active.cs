cc0891f9388485a90fae1cd68945b186e23a491a
Marek <84468808+marekvks@users.noreply.github.com>
2022-01-15 11:35:46 +0100

    Hint Trigger + Animation
    Animation -> Fade in, Fade out

---
+++ SSPSGameJam/Assets/Active.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Active : MonoBehaviour
+{
+    void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
--- SSPSGameJam/Assets/Scripts/ListTrigger.cs
+++ SSPSGameJam/Assets/Scripts/ListTrigger.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ListTrigger : MonoBehaviour
 {
+    public GameObject hint;
 
-    public LayerMask collectables;
+    public Animator animator;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision)
+        if (collision.tag == "Collectables")
         {
-            Debug.Log("Press E");
+            hint.SetActive(true);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        animator.SetBool("hide", true);
     }
 }
